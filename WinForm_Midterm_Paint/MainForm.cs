b96944e9ccbc95ee47e6893d8c876f7c7f5b130e
PcTrg <107632201+PcTrg@users.noreply.github.com>
2023-05-04 07:53:54 +0700

    Add files via upload

---
+++ WinForm_Midterm_Paint/MainForm.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Midterm_Paint
+{
+    public partial class MainForm : Form
+    {
+        private bool btnLine = false;
+        private bool btnEllipse = false;
+
+        private bool isDrawing = false;
+        private bool isMultiSelecting = false;
+
+        private ShapePos resizePos;
+
+        private List<Shape> shapeList = new List<Shape>();
+
+        private Pen newPen = new Pen(Color.Black, 5);
+
+        private void AddList(MouseEventArgs e)
+        {
+            if (btnLine)
+            {
+                Line newLine = new Line(e.Location, e.Location, newPen);
+                shapeList.Add(newLine);
+            }
+
+            if (btnEllipse)
+            {
+                Ellipse newEllipse = new Ellipse(e.Location, e.Location, newPen, 
+                    new Rectangle(e.Location, new Size(0,0)));
+                shapeList.Add(newEllipse);
+            }
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+            drawPanel.SetDoubleBuffered();
+        }
+
+        private void drawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Shape.DrawAllPath(shapeList, e);
+
+            for (int i = 0; i < shapeList.Count; i++)
+            {
+                shapeList[i].DrawShape(e);
+            }
+        }
+
+        private void drawPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (btnLine || btnEllipse)
+            {
+                isDrawing = true;
+                AddList(e);
+
+                drawPanel.Refresh();
+            }
+
+            if (!isDrawing)
+            {
+                // stop multiselect
+                bool select = false;
+                for (int i = 0; i < shapeList.Count; i++)
+                {
+                    if (shapeList[i].Path.IsVisible(e.Location)) select = true;
+                }
+                if (!select) isMultiSelecting = false;
+
+                // select, resize, move, unselect
+                for (int i = 0; i < shapeList.Count; i++)
+                {
+                    // select
+                    if (shapeList[i].Path.IsVisible(e.Location))
+                    {
+                        shapeList[i].IsSelected = true;
+                        drawPanel.Refresh();
+                    }
+
+                    //resize
+                    if (shapeList[i].IsSelected)
+                    {
+                        if (shapeList[i].ResizeTopLeft.IsVisible(e.Location))
+                        {
+                            shapeList[i].IsResizing = true;
+                            resizePos = ShapePos.TopLeft;
+                        }
+
+                        if (shapeList[i].ResizeTopRight.IsVisible(e.Location))
+                        {
+                            shapeList[i].IsResizing = true;
+                            resizePos = ShapePos.TopRight;
+                        }
+
+                        if (shapeList[i].ResizeBottomLeft.IsVisible(e.Location))
+                        {
+                            shapeList[i].IsResizing = true;
+                            resizePos = ShapePos.BottomLeft;
+                        }
+
+                        if (shapeList[i].ResizeBottomRight.IsVisible(e.Location))
+                        {
+                            shapeList[i].IsResizing = true;
+                            resizePos = ShapePos.BottomRight;
+                        }
+                    }
+
+                    if (shapeList[i].IsSelected)
+                    {
+                        //move
+                        if (shapeList[i].Path.IsVisible(e.Location) || isMultiSelecting)
+                        {
+                            shapeList[i].IsMoving = true;
+
+                            shapeList[i].Dis = new Point(
+                            e.Location.X - shapeList[i].P1.X,
+                            e.Location.Y - shapeList[i].P1.Y);
+                        }
+                        else if (!shapeList[i].IsResizing)
+                        {
+                            // multi select
+                            if (ModifierKeys == Keys.Control)
+                            {
+                                isMultiSelecting = true;
+                            }
+                            // unselect
+                            else
+                            {
+                                shapeList[i].IsSelected = false;
+                                isMultiSelecting = false;
+                                drawPanel.Refresh();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void drawPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                shapeList[shapeList.Count - 1].UpdateShape(ShapePos.BottomRight, e);
+                drawPanel.Refresh();
+            }
+            else
+            {
+                // resize
+                for (int i = 0; i < shapeList.Count; i++)
+                {
+                    if (shapeList[i].IsResizing)
+                    {
+                        shapeList[i].UpdateShape(resizePos, e);
+                        shapeList[i].UpdatePath(e);
+                        drawPanel.Refresh();
+                    }
+                }
+
+                // multiple move obj allow
+                for (int i = 0; i < shapeList.Count; i++)
+                {
+                    if (shapeList[i].IsMoving)
+                    {
+                        shapeList[i].MoveShape(e);
+                        shapeList[i].UpdatePath(e);
+                        drawPanel.Refresh();
+                    }
+                }
+            }
+        }
+
+        private void drawPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                shapeList[shapeList.Count - 1].UpdatePath(e);
+                drawPanel.Refresh();
+            }
+
+            // stop action
+            isDrawing = false;
+            for (int i = 0; i < shapeList.Count; i++)
+            {
+                shapeList[i].IsResizing = false;
+                shapeList[i].IsMoving = false;
+            }
+
+            // btn reset
+            btnLine = false;
+            btnEllipse = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnLine = true;
+
+            //other false
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            btnEllipse = true;
+        }
+    }
+
+    public static class ErrorFix
+    {
+        public static void SetDoubleBuffered(this Panel panel)
+        {
+            typeof(Panel).InvokeMember("DoubleBuffered",
+                BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty,
+                null, panel, new object[] { true });
+        }
+    }
+}
---
+++ WinForm_Midterm_Paint/Shape.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Midterm_Paint
+{
+    public enum ShapePos
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+    }
+
+    public abstract class Shape
+    {
+        private Point p1;
+        private Point p2;
+        private Point dis; // distance from mouse to p1
+
+        private Pen pen;
+        private Pen resizePen = new Pen(Color.DodgerBlue);
+
+        GraphicsPath path;
+        GraphicsPath resizeTopLeft;
+        GraphicsPath resizeTopRight;
+        GraphicsPath resizeBottomLeft;
+        GraphicsPath resizeBottomRight;
+
+        private bool isSelected = false;
+        private bool isResizing = false;
+        private bool isMoving = false;
+
+        public Point P1 { get => p1; set => p1 = value; }
+        public Point P2 { get => p2; set => p2 = value; }
+        public Pen Pen { get => pen; set => pen = value; }
+        public GraphicsPath Path { get => path; set => path = value; }
+        public GraphicsPath ResizeTopLeft { get => resizeTopLeft; set => resizeTopLeft = value; }
+        public GraphicsPath ResizeTopRight { get => resizeTopRight; set => resizeTopRight = value; }
+        public GraphicsPath ResizeBottomLeft { get => resizeBottomLeft; set => resizeBottomLeft = value; }
+        public GraphicsPath ResizeBottomRight { get => resizeBottomRight; set => resizeBottomRight = value; }
+        public bool IsSelected { get => isSelected; set => isSelected = value; }
+        public bool IsMoving { get => isMoving; set => isMoving = value; }
+        public Point Dis { get => dis; set => dis = value; }
+        public bool IsResizing { get => isResizing; set => isResizing = value; }
+
+        public Shape(Point p1, Point p2, Pen pen)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.pen = pen;
+        }
+
+        private void swap(Point a, Point b)
+        {
+            Point temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public void UpdatePath(MouseEventArgs e)
+        {
+            //restric P1<P2
+            if (p1.X > p2.X && p1.Y < p2.Y || 
+                p1.X > p2.X && p1.Y > p2.Y)
+            {
+                swap(p1, p2);
+            }
+            
+            // calculate path
+            Point start = new Point(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y));
+            Point end = new Point(Math.Max(p1.X, p2.X), Math.Max(p1.Y, p2.Y));
+
+            // add pic path
+            path = new GraphicsPath();
+
+            path.AddRectangle(
+                    new Rectangle(
+                        start, 
+                        new Size(Math.Abs(end.X - start.X), Math.Abs(end.Y - start.Y))));
+
+            // add resize path
+            resizeTopLeft = new GraphicsPath();
+            resizeTopRight = new GraphicsPath();
+            resizeBottomLeft = new GraphicsPath();
+            resizeBottomRight = new GraphicsPath();
+
+            resizeTopLeft.AddRectangle(
+                    new Rectangle(
+                        new Point(start.X-10, start.Y- 10),
+                        new Size(20,20)));
+            resizeTopRight.AddRectangle(
+                    new Rectangle(
+                        new Point(end.X - 10, start.Y - 10),
+                        new Size(20, 20)));
+            resizeBottomLeft.AddRectangle(
+                    new Rectangle(
+                        new Point(start.X - 10, end.Y - 10),
+                        new Size(20, 20)));
+            resizeBottomRight.AddRectangle(
+                    new Rectangle(
+                        new Point(end.X - 10, end.Y - 10),
+                        new Size(20, 20)));
+        }
+        public virtual void UpdateShape(ShapePos resizePos, MouseEventArgs e)
+        {
+            switch (resizePos)
+            {
+                case ShapePos.TopLeft:
+                    {
+                        p1 = e.Location;
+                        break;
+                    }
+                case ShapePos.TopRight:
+                    {
+                        p1 = new Point(p1.X, e.Location.Y);
+                        p2 = new Point(e.Location.X, p2.Y);
+                        break;
+                    }
+                case ShapePos.BottomLeft:
+                    {
+                        p1 = new Point(e.Location.X, p1.Y);
+                        p2 = new Point(p2.X, e.Location.Y);
+                        break;
+                    }
+                case ShapePos.BottomRight:
+                    {
+                        p2 = e.Location;
+                        break;
+                    }
+            }
+        }
+        public virtual void MoveShape(MouseEventArgs e)
+        {
+            Point originalDis_From_P1toP2 = new Point(p2.X - p1.X, p2.Y - p1.Y);
+
+            p1 = new Point(e.Location.X - dis.X, e.Location.Y - dis.Y);
+            p2 = new Point(p1.X + originalDis_From_P1toP2.X, p1.Y + originalDis_From_P1toP2.Y);
+        }
+        public virtual void DrawShape(PaintEventArgs e)
+        {
+
+        }
+
+        public static void DrawAllPath(List<Shape> shapList, PaintEventArgs e)
+        {
+            for(int i = 0; i < shapList.Count; i++)
+            {
+                if (shapList[i].isSelected)
+                {
+                    // draw the path
+                    e.Graphics.DrawPath(shapList[i].resizePen, shapList[i].path);
+
+                    // draw 4 resize path
+                    e.Graphics.DrawPath(shapList[i].resizePen, shapList[i].resizeTopLeft);
+                    e.Graphics.DrawPath(shapList[i].resizePen, shapList[i].resizeTopRight);
+                    e.Graphics.DrawPath(shapList[i].resizePen, shapList[i].resizeBottomLeft);
+                    e.Graphics.DrawPath(shapList[i].resizePen, shapList[i].resizeBottomRight);
+                }
+            }
+        }
+    }
+
+    public class Line: Shape
+    {
+        public Line(Point p1, Point p2, Pen pen): base(p1, p2, pen)
+        {
+
+        }
+
+        public override void DrawShape(PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(Pen, P1, P2);
+        }
+        public override void MoveShape(MouseEventArgs e)
+        {
+            base.MoveShape(e);
+        }
+        public override void UpdateShape(ShapePos resizePos, MouseEventArgs e)
+        {
+            base.UpdateShape(resizePos, e);
+        }
+    }
+    public class Ellipse: Shape
+    {
+        private Rectangle ellipseRect;
+        public Rectangle EllipseRect { get => ellipseRect; set => ellipseRect = value; }
+
+        public Ellipse(Point p1, Point p2, Pen pen, Rectangle ellipseRect) : base(p1, p2, pen)
+        {
+            this.ellipseRect = ellipseRect;
+        }
+
+        private void UpdateRect()
+        {
+            ellipseRect = new Rectangle(P1, new Size(P2.X - P1.X, P2.Y - P1.Y));
+        }
+
+        public override void DrawShape(PaintEventArgs e)
+        {
+            UpdateRect();
+            e.Graphics.DrawEllipse(Pen, ellipseRect);
+        }
+        public override void UpdateShape(ShapePos resizePos, MouseEventArgs e)
+        {
+            base.UpdateShape(resizePos, e);
+            UpdateRect();
+        }
+        public override void MoveShape(MouseEventArgs e)
+        {
+            base.MoveShape(e);
+            UpdateRect();
+        }
+    }
+}
